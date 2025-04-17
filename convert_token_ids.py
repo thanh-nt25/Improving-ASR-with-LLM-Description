19ecead3f44d2a7d8c84aca4a9437a82ede5dabe
thanh-nt25 <thanhnguyenqwe65@gmail.com>
2025-04-17 17:34:39 +0700

    fixed

--- convert_token_ids.py
+++ convert_token_ids.py
@@ -1,12 +1,12 @@
 from transformers import GPT2Tokenizer
 
+from transformers_prompt import Seq2SeqTrainingArguments, Seq2SeqTrainer, WhisperPromptForConditionalGeneration, GenerationConfig, WhisperFeatureExtractor, WhisperTokenizer, WhisperProcessor
 # Tải tokenizer của GPT-2 (hoặc mô hình mà bạn đang sử dụng)
 tokenizer = WhisperTokenizer.from_pretrained(f'openai/whisper-base.en', language='en', task='transcribe')
 
-print(tokenizer.get_vocab())
+# print(tokenizer.get_vocab())
 
-# Chuyển đổi token ID 50257 thành văn bản
-# token_id = 50257
-# decoded_token = tokenizer.decode([token_id])
+token_id = 50360
+decoded_token = tokenizer.decode([token_id])
 
-# print(f"Token ID {token_id} corresponds to: {decoded_token}")
+print(f"Token ID {token_id} corresponds to: {decoded_token}")
